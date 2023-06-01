20f2236caa258c3df5e1015a322c276f0a7193b6
Behnam <behnamborhani95@gmail.com>
2023-06-01 08:59:54 +0330

    base code created

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* preloader starts */
+const preloaderElem = document.querySelector(".preloader");
+
+window.addEventListener("load", function () {
+   preloaderElem.classList.add("hidden");
+});
+/* preloader ends */
